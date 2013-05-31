a06694c87f70c76b9545d424c94f3102d1facafa
after12am <me.after.12am@gmail.com>
2013-05-31 23:59:43 +0900

    bug of intersection. have to add to $.fn.queue

--- src/origin.js
+++ src/origin.js
@@ -1,16 +1,20 @@
 // transform origin is shifted after the origin have been applied to dom element.
 // confirmed in Chrome.
 $.fn.origin = function(top, left) {
-    var params = {};
-    params[browser.css.property('origin')] = str('{0} {1}').format(top, left);
-    params[browser.css.property('duration')] = '0';
-    $(this).css(params);
-    var i = 0;
-    // We have to wait until css property is set.
-    // If not so, next queue might be executed before setting css to dom.
-    while (1) {
-        if ($(this).css(browser.css.property('duration')).match(/^0/)) break;
-        if (++i > 50) break; // avoid infinite loop
-    }
+    var that = this;
+    $(this).queue(function() {
+        var params = {};
+        params[browser.css.property('origin')] = str('{0} {1}').format(top, left);
+        params[browser.css.property('duration')] = '0';
+        $(this).css(params);
+        var i = 0;
+        // We have to wait until css property is set.
+        // If not so, next queue might be executed before setting css to dom.
+        while (1) {
+            if ($(this).css(browser.css.property('duration')).match(/^0/)) break;
+            if (++i > 50) break; // avoid infinite loop
+        }
+        $(that).dequeue();
+    });
     return this;
 }
--- src/perspective.js
+++ src/perspective.js
@@ -2,13 +2,17 @@
     Whether you use perspective. If you want to make perspective disable, set false on depth.
 */
 $.fn.perspective = function(depth) {
-    $(this).parent().css(browser.css.property('perspective'), str('{0}px').format(+depth || 0));
-    var i = 0;
-    // We have to wait until css property is set.
-    // If not so, next queue might be executed before setting css to dom.
-    while (1) {
-        if ($(this).parent().css(browser.css.property('duration')).match(/^0/)) break;
-        if (++i > 50) break; // avoid infinite loop
-    }
+    var that = this;
+    $(this).queue(function() {
+        $(this).parent().css(browser.css.property('perspective'), str('{0}px').format(+depth || 0));
+        var i = 0;
+        // We have to wait until css property is set.
+        // If not so, next queue might be executed before setting css to dom.
+        while (1) {
+            if ($(this).parent().css(browser.css.property('duration')).match(/^0/)) break;
+            if (++i > 50) break; // avoid infinite loop
+        }
+        $(that).dequeue();
+    });
     return this;
 }
