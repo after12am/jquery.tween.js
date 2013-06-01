023646f53032d405766c6cdf2a46e722f85b9b75
after12am <me.after.12am@gmail.com>
2013-06-01 09:47:12 +0900

    bug of applied

--- src/origin.js
+++ src/origin.js
@@ -1,20 +1,18 @@
-// transform origin is shifted after the origin have been applied to dom element.
-// confirmed in Chrome.
+/*
+    The transform-origin has to applied to element before apply any other transformation.
+    If you don't do so, transform-origin of element would have shifted a little.
+*/
 $.fn.origin = function(top, left) {
-    var that = this;
     $(this).queue(function() {
-        var params = {};
-        params[browser.css.property('origin')] = str('{0} {1}').format(top, left);
-        params[browser.css.property('duration')] = '0';
-        $(this).css(params);
+        $(this).css(browser.css.property('origin'), str('{0} {1}').format(top, left));
         var i = 0;
         // We have to wait until css property is set.
         // If not so, next queue might be executed before setting css to dom.
         while (1) {
-            if ($(this).css(browser.css.property('duration')).match(/^0/)) break;
+            if ($(this).css(browser.css.property('origin'))) break;
             if (++i > 50) break; // avoid infinite loop
         }
-        $(that).dequeue();
+        $(this).dequeue();
     });
     return this;
 }
--- src/perspective.js
+++ src/perspective.js
@@ -2,17 +2,16 @@
     Whether you use perspective. If you want to make perspective disable, set false on depth.
 */
 $.fn.perspective = function(depth) {
-    var that = this;
     $(this).queue(function() {
         $(this).parent().css(browser.css.property('perspective'), str('{0}px').format(+depth || 0));
         var i = 0;
         // We have to wait until css property is set.
         // If not so, next queue might be executed before setting css to dom.
         while (1) {
-            if ($(this).parent().css(browser.css.property('duration')).match(/^0/)) break;
+            if ($(this).parent().css(browser.css.property('perspective'))) break;
             if (++i > 50) break; // avoid infinite loop
         }
-        $(that).dequeue();
+        $(this).dequeue();
     });
     return this;
 }
