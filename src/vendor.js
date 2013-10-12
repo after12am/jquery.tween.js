48b528f003d24178c2064dd58a158470d266e1c4
after12am <me.after.12am@gmail.com>
2013-10-13 00:11:12 +0900

    tested IE6-8

--- src/vendor.js
+++ src/vendor.js
@@ -57,3 +57,16 @@ var transitionEnd = (function() {
   if ('transitionend' in e.style) return 'transitionend';
   return;
 })();
+
+// IE6-8 does not support Object.create
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+}
